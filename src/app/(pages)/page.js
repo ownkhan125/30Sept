fe76ef86cb1033977ab165702544053a07497581
ownkhan125 <107195320+ownkhan125@users.noreply.github.com>
2024-10-17 15:08:39 +0500

    update

--- src/app/(pages)/page.js
+++ src/app/(pages)/page.js
@@ -30,7 +30,7 @@ const page = () => {
         <div className="flex items-baseline">
           <div className="w-[80%] px-5">
             <div className=" w-full bg-[#e7e7e7] border-x border-slate-300 rounded-md shadow-md p-10 ">
-              <div className="w-fit mx-auto"><h2>POST</h2></div>
+              <div className="w-full sticky top-0 bg-[#e7e7e7]"><h2 className="text-center">POST</h2></div>
               {
                 data.map((items, index) => (
                   <div key={index} className="bg-white border-zinc-100 rounded-md p-3 m-2 shadow-md">
@@ -46,8 +46,8 @@ const page = () => {
           </div>
 
 
-          <div className="w-[20%] h-[100vh]">
-            <div className='sticky top-0 w-full bg-[#e7e7e7] rounded-lg p-10 shadow-md animate-pulse'>
+          <div className="w-[20%] sticky top-0">
+            <div className=' w-full bg-[#e7e7e7] rounded-lg p-10 shadow-md animate-pulse'>
               <Link href={'/login'}>< button className="btn my-3">Login</button></Link>
               <Link href={'/signup'}>< button className="btn my-3">Sign up</button></Link>
             </div>
